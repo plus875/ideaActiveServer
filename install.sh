ddadfa42a2a8ded5309f323a47c8fca99a3b1418
wzonxin <blade8.pm@gmail.com>
2021-09-01 14:36:00 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 echo "download LicenseServer file"
 wget https://github.com/wzonxin/ideaActiveServer/raw/master/IntelliJIDEALicenseServer_linux_amd64
-chmod +x $pwd/IntelliJIDEALicenseServer_linux_amd64
-mv $pwd/IntelliJIDEALicenseServer_linux_amd64 /usr/bin/IntelliJIDEALicenseServer_linux_amd64
+chmod +x ./IntelliJIDEALicenseServer_linux_amd64
+mv ./IntelliJIDEALicenseServer_linux_amd64 /usr/bin/IntelliJIDEALicenseServer_linux_amd64
 
 echo "download service file"
 
 wget https://github.com/wzonxin/ideaActiveServer/raw/master/IntelliJIDEALicenseServer.service
-mv $pwd/IntelliJIDEALicenseServer.service /usr/lib/systemd/system/IntelliJIDEALicenseServer.service
+mv ./IntelliJIDEALicenseServer.service /usr/lib/systemd/system/IntelliJIDEALicenseServer.service
 sudo systemctl enable IntelliJIDEALicenseServer.service
 sudo systemctl start IntelliJIDEALicenseServer.service
 
